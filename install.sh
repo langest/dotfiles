6be14e0fdbe4b1ffde15f928dd4140ca9b9eed48
Daniel Langesten <daniel.langest@gmail.com>
2015-07-02 10:13:06 +0200

    added installation of dwm

--- install.sh
+++ install.sh
@@ -10,4 +10,21 @@ echo    # move to a new line
 echo '=== Starting to install useful programs ==='
 echo '===      running "sudo pacman -S"       ==='
 sudo pacman -S $TOOLS $LANGS
+
+echo    # move to a new line
+echo '=== Installing dwm ==='
+read -p "Clone and install dwm from 'github.com/langest/dwm'? [YyNn]" -n 1 -r
+while [[ $REPLY =~ ^[^YyNn]$ ]]
+do
+	read -n 1 -r
+done
+
+echo    # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	git clone git@github.com:langest/$HOME/programming/c/dwm
+	make -C $HOME/programming/c/dwm config.h
+	sudo make -C $HOME/programming/c/dwm install
+fi
+
 echo 'Done'
