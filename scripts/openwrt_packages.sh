05af8636a271cbbb715b14130b3cbe77b258eea2
Daniel Langesten <daniel.langest@gmail.com>
2022-08-01 18:02:49 +0200

    Add install script for openwrt

---
+++ scripts/openwrt_packages.sh
@@ -0,0 +1,17 @@
+opkg update
+opkg upgrade // TODO fix
+
+opkg install luci-proto-wireguard
+
+opkg install kmod-usb-storage
+opkg install kmod-usb-storage-uas
+opkg install usbutils
+opkg install block-mount
+opkg install libblkid
+opkg install e2fsprogs
+opkg install kmod-fs-ext4
+opkg install kmod-fs-exfat
+opkg install f2fs-tools
+opkg install kmod-fs-f2fs
+
+opkg install bash
