938c51e9cfc3cd249b4f0a9b24263635e3dabdd5
Daniel Langesten <daniel.langest@gmail.com>
2023-02-04 09:18:11 +0100

    Update test vpn script

--- scripts/vpn_rotation.sh
+++ scripts/vpn_rotation.sh
@@ -6,6 +6,33 @@ declare -a VPN_INTERFACES=(
 	"IF_NAME_2"
 )
 
+print_usage() {
+	echo "This is a simple script that can test if the current connection"
+	echo "is working and rotate to a different vpn interface is necessary."
+	echo "This program will create or overwrite a file with the current interface used."
+	echo "By default it will:"
+	echo "  Rotate on a failed test."
+	echo '  Use a file "/tmp/current_interface.txt" to track current interface.'
+	echo ""
+	echo "Usage: $0 [OPTION]"
+	echo "Options:"
+	echo "  --help   | -h Print this help message"
+	echo "  --rotate | -r Force a vpn rotation without testing if it works"
+	echo "Example: $0 --force"
+}
+
+if [[ $# -gt 1 || $1 == '--help' || $1 == '-h' ]]; then
+	print_usage
+	exit 1
+fi
+
+VALID_FLAGS=("--help", "-h", "--rotate", "-r")
+FLAG="$1"
+if [[ ! ${VALID_FLAGS[*]} =~ $FLAG ]]; then
+	echo "Invalid flag: ${FLAG}"
+	print_usage
+	exit 1
+fi
 
 function change_interface() {
 	prev_interface=$(<current_interface.txt)
@@ -18,7 +45,7 @@ function change_interface() {
 	done
 
 	ifup $next_interface
-	echo $next_interface>"current_interface.txt"
+	echo $next_interface>"/tmp/current_interface.txt"
 
 	# Stop current interface
 	if [[ -z $current_interface ]]; then
@@ -42,7 +69,10 @@ function test_vpn() {
 	return 1
 }
 
-test_vpn
+$? = 0
+if [[  $1 != '--rotate' && $1 != '-r' ]]; then
+	test_vpn
+fi
 if [[ $? -ne 0 ]]; then
 	echo Changing interface
 	change_interface
