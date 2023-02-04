396270b02336b551c9efc842b2877f24cb351056
Daniel Langesten <daniel.langest@gmail.com>
2023-02-04 09:17:11 +0100

    Add some delay when setting up screens

--- scripts/setup_screens.sh
+++ scripts/setup_screens.sh
@@ -8,6 +8,12 @@ if [[ $MONITOR_SETTINGS = "external" ]]; then
 	sleep 1.3
 	xrandr --output HDMI2 --mode 1920x1200
 	xrandr --output HDMI1 --left-of HDMI2 --mode 1920x1200 --rotate left
+else
+	xrandr --output eDP1 --auto
+	sleep 0.1
+	xrandr --output HDMI2 --off
+	sleep 0.1
+	xrandr --output HDMI1 --off
 fi
 
 bash `dirname "$0"`/wallpaper.sh
