288aa56492b958480e0473c7a199fa0cba276219
Daniel Langesten <daniel@langesten.com>
2023-06-10 22:18:16 +0200

    Fix screen layout

--- scripts/setup_screens.sh
+++ scripts/setup_screens.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#feh --randomize --recursive --bg-fill /home/langest/img/wps
-#/home/langest/.fehbg
+HDMI1_position="1200x361"
+HDMI2_position="0x0"
 
 if [[ $MONITOR_SETTINGS = "external" ]]; then
 	xrandr --output eDP1 --off
 	sleep 1.3
-	xrandr --output HDMI1 --mode 1920x1200
-	xrandr --output HDMI2 --left-of HDMI1 --mode 1920x1200 --rotate left
+	xrandr --output HDMI1 --mode 1920x1200 --pos $HDMI1_position
+	xrandr --output HDMI2 --mode 1920x1200 --rotate left --pos $HDMI2_position
 else
 	xrandr --output eDP1 --auto
 	sleep 0.1
@@ -17,3 +17,4 @@ else
 fi
 
 bash `dirname "$0"`/wallpaper.sh
+
