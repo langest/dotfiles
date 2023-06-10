65fbd69f64d8b1a753560e6140c5465bc5a58946
Daniel Langesten <daniel@langesten.com>
2023-06-10 21:41:56 +0200

    Update xfce wallpaper script

--- scripts/xfce_wallpaper.sh
+++ scripts/xfce_wallpaper.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export DISPLAY=:0.0
+export XAUTHORITY="/home/daniel/.Xauthority"
+PID=$(pgrep xfce4-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+
 DIR_H="${HOME}/Pictures/wps"
 DIR_V="${HOME}/Pictures/wps_portrait"
 
@@ -7,15 +12,13 @@ PIC1=$(find "$DIR_H" -type f \( -name '*.jpg' -o -name '*.png' \) | shuf -n 1)
 PIC2=$(find "$DIR_H" -type f \( -name '*.jpg' -o -name '*.png' \) | shuf -n 1)
 PIC3=$(find "$DIR_V" -type f \( -name '*.jpg' -o -name '*.png' \) | shuf -n 1)
 
-echo "$PIC1"
-echo "$PIC2"
-echo "$PIC3"
+#echo `date` 'Updating background' >> /home/daniel/.log/wallpaper.log
 
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitoreDP1/workspace1/last-image -s "$PIC1"
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitoreDP1/workspace1/image-style -s 5
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitoreDP1/workspace1/last-image -s "$PIC1"
 
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorHDMI1/workspace1/last-image -s "$PIC2"
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorHDMI1/workspace1/image-style -s 5
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorHDMI1/workspace1/last-image -s "$PIC2"
 
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorHDMI2/workspace1/last-image -s "$PIC3"
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorHDMI2/workspace1/image-style -s 5
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorHDMI2/workspace1/last-image -s "$PIC3"
