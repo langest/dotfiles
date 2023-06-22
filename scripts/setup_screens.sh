a8ce5ee8f022b24d1c790a7e0f723473daf07889
Daniel Langesten <daniel@langesten.com>
2023-06-22 21:09:14 +0200

    Declare primary screen

--- scripts/setup_screens.sh
+++ scripts/setup_screens.sh
@@ -6,10 +6,10 @@ HDMI2_position="0x0"
 if [[ $MONITOR_SETTINGS = "external" ]]; then
 	xrandr --output eDP1 --off
 	sleep 1.3
-	xrandr --output HDMI1 --mode 1920x1200 --pos $HDMI1_position
+	xrandr --output HDMI1 --mode 1920x1200 --pos $HDMI1_position --primary
 	xrandr --output HDMI2 --mode 1920x1200 --rotate left --pos $HDMI2_position
 else
-	xrandr --output eDP1 --auto
+	xrandr --output eDP1 --auto --primary
 	sleep 0.1
 	xrandr --output HDMI1 --off
 	sleep 0.1
