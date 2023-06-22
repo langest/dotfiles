9ba647f85b299301924906b6078b67beee20bc2d
Daniel Langesten <daniel@langesten.com>
2023-06-22 16:15:43 +0200

    Update battery script

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -23,32 +23,32 @@ fi
 
 if [[ $CMD = "reset" ]]; then
 	echo "Setting default values"
-	tpacpi-bat -s ST 1 0
-	tpacpi-bat -s SP 1 0
-	tpacpi-bat -s ST 2 0
-	tpacpi-bat -s SP 2 0
+	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s ST 1 0
+	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s SP 1 0
+	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s ST 2 0
+	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s SP 2 0
 fi
 
 if [[ $CMD = "balanced" ]]; then
 	echo "Setting battery friendly values"
-	tpacpi-bat -s ST 1 38
-	tpacpi-bat -s SP 1 45
-	tpacpi-bat -s ST 2 38
-	tpacpi-bat -s SP 2 55
+	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s ST 1 38
+	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s SP 1 45
+	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s ST 2 38
+	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s SP 2 55
 fi
 
 if [[ $CMD = "hot" ]]; then
 	echo "Charging external battery som extra"
-	tpacpi-bat -s ST 1 38
-	tpacpi-bat -s SP 1 45
-	tpacpi-bat -s ST 2 71
-	tpacpi-bat -s SP 2 73
+	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s ST 1 38
+	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s SP 1 45
+	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s ST 2 71
+	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s SP 2 73
 fi
 
 echo "Battery 1 limits"
-tpacpi-bat -g ST 1
-tpacpi-bat -g SP 1
+/home/daniel/repos/tpacpi-bat/tpacpi-bat -g ST 1
+/home/daniel/repos/tpacpi-bat/tpacpi-bat -g SP 1
 echo "Battery 2 limits"
-tpacpi-bat -g ST 2
-tpacpi-bat -g SP 2
+/home/daniel/repos/tpacpi-bat/tpacpi-bat -g ST 2
+/home/daniel/repos/tpacpi-bat/tpacpi-bat -g SP 2
 exit 0
