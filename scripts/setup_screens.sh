f9f533102feaa2a995602ea0eb8a19a04ae27ef3
Daniel Langesten <daniel@langesten.com>
2023-06-25 11:01:21 +0200

    Update config for i3

--- scripts/setup_screens.sh
+++ scripts/setup_screens.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-HDMI1_position="1200x361"
-HDMI2_position="0x0"
+HDMI1_position="0x0"
+HDMI2_position="-1200x-361"
 
 if [[ $MONITOR_SETTINGS = "external" ]]; then
 	xrandr --output eDP1 --off
-	sleep 1.3
+	sleep 1.4
 	xrandr --output HDMI1 --mode 1920x1200 --pos $HDMI1_position --primary
+	sleep 0.3
 	xrandr --output HDMI2 --mode 1920x1200 --rotate left --pos $HDMI2_position
 else
 	xrandr --output eDP1 --auto --primary
