ca48963d62a89162c03c7a81d300d252fa09c2c9
Daniel Langesten <daniel@langesten.com>
2023-10-01 08:26:13 +0200

    Convert battery script to use tlp

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,54 +1,45 @@
 #!/bin/bash
 
-if [[ $# -eq 2 ]]; then
-	echo "Usage: $0 <reset|balanced|status>"
-	echo "Example: $0 on"
-	exit 1
+# Check for the correct number of arguments and root privileges
+if [[ $# -ne 1 ]] || [[ "$EUID" -ne 0 ]]; then
+    echo "Usage: $0 <reset|balanced|hot|status>"
+    echo "Run as root."
+    exit 1
 fi
 
-if [[ "$EUID" -ne 0 ]]; then
-	echo "Run as root"
-	exit 1
+# Main logic based on the command
+case "$1" in
+    reset)
+        echo "Setting default values"
+        tlp setcharge 0 100 BAT0
+        tlp setcharge 0 100 BAT1
+        ;;
+    balanced)
+        echo "Setting battery-friendly values"
+        tlp setcharge 38 45 BAT0
+        tlp setcharge 38 55 BAT1
+        ;;
+    hot)
+        echo "Charging external battery some extra"
+        tlp setcharge 38 45 BAT0
+        tlp setcharge 71 73 BAT1
+        ;;
+    status)
+        tlp-stat -b
+        exit 0
+        ;;
+    *)
+        echo "Invalid command."
+        echo "Requires parameter reset, balanced, hot, or status"
+        exit 1
+        ;;
+esac
+
+# Apply TLP settings, unless the command is 'status'
+if [[ "$1" != "status" ]]; then
+    echo "Applying changes..."
+    tlp start
 fi
 
-
-CMD="$1"
-if [[ ! ($CMD = "reset" || $CMD = "balanced" || $CMD = "hot" || $CMD = "status") ]]; then
-	echo "Requires parameter reset, balanced, hot or status"
-	echo "Usage: $0 <reset|balanced|hot|status>"
-	echo "hot: charges external battery more than balanced"
-	echo "Example: $0 reset"
-	exit 1
-fi
-
-if [[ $CMD = "reset" ]]; then
-	echo "Setting default values"
-	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s ST 1 0
-	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s SP 1 0
-	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s ST 2 0
-	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s SP 2 0
-fi
-
-if [[ $CMD = "balanced" ]]; then
-	echo "Setting battery friendly values"
-	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s ST 1 38
-	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s SP 1 45
-	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s ST 2 38
-	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s SP 2 55
-fi
-
-if [[ $CMD = "hot" ]]; then
-	echo "Charging external battery som extra"
-	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s ST 1 38
-	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s SP 1 45
-	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s ST 2 71
-	/home/daniel/repos/tpacpi-bat/tpacpi-bat -s SP 2 73
-fi
-
-echo "Battery 1 limits"
-/home/daniel/repos/tpacpi-bat/tpacpi-bat -g ST 1
-/home/daniel/repos/tpacpi-bat/tpacpi-bat -g SP 1
-echo "Battery 2 limits"
-/home/daniel/repos/tpacpi-bat/tpacpi-bat -g ST 2
-/home/daniel/repos/tpacpi-bat/tpacpi-bat -g SP 2
 exit 0
+
