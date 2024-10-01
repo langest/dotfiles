19715672a6cbaec3f4d08a25603cd04477d4a151
Daniel Langesten <daniel@langesten.com>
2024-10-01 19:13:27 +0200

    Add screen layout

--- scripts/setup_screens.sh
+++ scripts/setup_screens.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -e
+set -x
 
-HDMI1_position="0x0"
-HDMI2_position="-1200x-362"
+HDMI1_position="1200x355"
+HDMI2_position="0x0"
+eDP1_position="3120x475"
 
 if [[ $MONITOR_SETTINGS = "external" ]]; then
 	xrandr --output eDP1 --off
@@ -9,6 +12,18 @@ if [[ $MONITOR_SETTINGS = "external" ]]; then
 	xrandr --output HDMI1 --mode 1920x1200 --pos $HDMI1_position --primary
 	sleep 0.3
 	xrandr --output HDMI2 --mode 1920x1200 --rotate left --pos $HDMI2_position
+elif [[ $MONITOR_SETTINGS = "all" ]]; then
+	xrandr --output eDP1 --mode 1920x1080 --pos $eDP1_position
+	xrandr
+	sleep 2.4
+
+	xrandr --output HDMI1 --mode 1920x1200 --pos $HDMI1_position --primary
+	xrandr
+	sleep 2.4
+
+	echo xrandr --output HDMI2 --mode 1920x1200 --rotate left --pos $HDMI2_position
+	xrandr --output HDMI2 --mode 1920x1200 --rotate left --pos $HDMI2_position
+	xrandr
 else
 	xrandr --output eDP1 --auto --primary
 	sleep 0.1
--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+# Set wallpaper based on the MONITOR_SETTINGS variable
+
 if [[ $MONITOR_SETTINGS = "external" ]]; then
-	# Oneliner that does the same thing
+	# Randomly select one wallpaper for landscape and one for portrait
+	LANDSCAPE=$(find $HOME/Pictures/wps/ -type f | sort -R | head -n1)
+	PORTRAIT=$(find $HOME/Pictures/wps_portrait/ -type f | sort -R | head -n1)
 
-	# cat <(find $HOME/img/wps_portrait/ -type f -printf '"%p"\n' | sort -R | head -n1) <(find $HOME/img/wps/ -type f -printf '"%p"\n' | sort -R | head -n1) | xargs feh --bg-fill
-	LANDSCAPE=`find $HOME/Pictures/wps/ -type f | sort -R | head -n1`
-
-	PORTRAIT=`find $HOME/Pictures/wps_portrait/ -type f | sort -R | head -n1`
-
+	# Set the landscape and portrait wallpapers for external monitors
 	feh --bg-fill "$LANDSCAPE" "$PORTRAIT"
+
+	exit 0
+elif [[ $MONITOR_SETTINGS = "all" ]]; then
+	# Select one wallpaper each for eDP1, HDMI1 (landscape), and HDMI2 (portrait)
+	EDP_WALLPAPER=$(find $HOME/Pictures/wps/ -type f | sort -R | head -n1)
+	LANDSCAPE=$(find $HOME/Pictures/wps/ -type f | sort -R | head -n1)
+	PORTRAIT=$(find $HOME/Pictures/wps_portrait/ -type f | sort -R | head -n1)
+
+	# Set wallpapers for eDP1, HDMI1, and HDMI2
+	feh --bg-fill "$EDP_WALLPAPER" "$LANDSCAPE" "$PORTRAIT"
 
 	exit 0
 fi
 
+# Default behavior: randomize wallpapers from the wps folder
 feh --randomize --recursive --bg-fill $HOME/Pictures/wps
+
