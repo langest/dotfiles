e79e5a9c3fd3a7ea110497c5d5d5f551ef466697
Daniel Langesten <daniel@langesten.com>
2025-11-02 07:26:24 +0100

    Update wallpaper script for x260

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -2,24 +2,21 @@
 
 # Set wallpaper based on the MONITOR_SETTINGS variable
 
-if [[ $MONITOR_SETTINGS = "external" ]]; then
-	# Randomly select one wallpaper for landscape and one for portrait
-	LANDSCAPE=$(find $HOME/Pictures/wps/ -type f | sort -R | head -n1)
-	PORTRAIT=$(find $HOME/Pictures/wps_portrait/ -type f | sort -R | head -n1)
-
-	# Set the landscape and portrait wallpapers for external monitors
-	feh --bg-fill "$LANDSCAPE" "$PORTRAIT"
-
-	exit 0
-elif [[ $MONITOR_SETTINGS = "all" ]]; then
+if [ "$MONITOR_SETTINGS" = "external" ]; then
 	# Select one wallpaper each for eDP1, HDMI1 (landscape), and HDMI2 (portrait)
-	EDP_WALLPAPER=$(find $HOME/Pictures/wps/ -type f | sort -R | head -n1)
-	LANDSCAPE=$(find $HOME/Pictures/wps/ -type f | sort -R | head -n1)
 	PORTRAIT=$(find $HOME/Pictures/wps_portrait/ -type f | sort -R | head -n1)
+	LANDSCAPE_0=$(find $HOME/Pictures/wps/ -type f | sort -R | head -n1)
 
-	# Set wallpapers for eDP1, HDMI1, and HDMI2
-	feh --bg-fill "$EDP_WALLPAPER" "$LANDSCAPE" "$PORTRAIT"
+	feh --bg-fill "$LANDSCAPE_0" "$PORTRAIT"
+	exit 0
+fi
+if [ "$MONITOR_SETTINGS" = "all" ]; then
+	# Select one wallpaper each for eDP1, HDMI1 (landscape), and HDMI2 (portrait)
+	PORTRAIT=$(find $HOME/Pictures/wps_portrait/ -type f | sort -R | head -n1)
+	LANDSCAPE_0=$(find $HOME/Pictures/wps/ -type f | sort -R | head -n1)
+	LANDSCAPE_1=$(find $HOME/Pictures/wps/ -type f | sort -R | head -n1)
 
+	feh --bg-fill "$LANDSCAPE_0" "$LANDSCAPE_1" "$PORTRAIT"
 	exit 0
 fi
 
