613a53c09a845c2b25eca982616b9b5a0bfd38e5
Daniel Langesten <daniel@langesten.com>
2025-11-02 07:25:05 +0100

    Update set volume script

--- scripts/change_volume.sh
+++ scripts/change_volume.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-# Fetch current volume
-current_volume=$(amixer get Master | grep 'Mono:' | awk '{print $4}' | sed 's/\[\(.*\)%\]/\1/')
+# Get the volume using amixer and try different patterns
+get_volume() {
+    # Try different patterns that might appear in amixer output
+    volume=$(amixer get Master | grep -E '\[([0-9]+)%\]' | head -n 1 | sed -r 's/.*\[([0-9]+)%\].*/\1/')
+    echo "${volume:-0}"  # Return 0 if volume is empty
+}
 
-if [ "$1" = "increase" ]; then
-    if [ $current_volume -lt 95 ]; then
-        amixer set Master 5%+
-    else
-        amixer set Master 100%
-    fi
-elif [ "$1" = "decrease" ]; then
-    if [ $current_volume -gt 5 ]; then
-        amixer set Master 5%-
-    else
-        amixer set Master 0%
-    fi
-fi
+current_volume=$(get_volume)
 
+case "$1" in
+    "increase")
+        if [ "$current_volume" -lt 95 ]; then
+            amixer -q set Master 5%+
+        else
+            amixer -q set Master 100%
+        fi
+        ;;
+    "decrease")
+        if [ "$current_volume" -gt 5 ]; then
+            amixer -q set Master 5%-
+        else
+            amixer -q set Master 0%
+        fi
+        ;;
+    *)
+        echo "Usage: $0 {increase|decrease}"
+        exit 1
+        ;;
+esac
