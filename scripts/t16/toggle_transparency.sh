f0d81eab7eb82a49256ac3afe57fd20546f7bc1a
Daniel Langesten <daniel@langesten.com>
2025-12-05 20:24:50 +0100

    Add opacity step

--- scripts/t16/toggle_transparency.sh
+++ scripts/t16/toggle_transparency.sh
@@ -6,8 +6,12 @@ if swaymsg -t get_tree | jq -e '.. | select(.focused? == true) | .marks[]? | sel
   swaymsg unmark opacity_1
   swaymsg mark opacity_2
 elif swaymsg -t get_tree | jq -e '.. | select(.focused? == true) | .marks[]? | select(. == "opacity_2")' > /dev/null; then
-  swaymsg opacity 1
+  swaymsg opacity 0.6
   swaymsg unmark opacity_2
+  swaymsg mark opacity_3
+elif swaymsg -t get_tree | jq -e '.. | select(.focused? == true) | .marks[]? | select(. == "opacity_3")' > /dev/null; then
+  swaymsg opacity 1
+  swaymsg unmark opacity_3
 else
   swaymsg opacity 0.87
   swaymsg mark opacity_1
